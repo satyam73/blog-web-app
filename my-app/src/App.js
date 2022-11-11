0b7d86123969ea9651c38c0d42500b39f620cf44
Satyam <bajpai1973satyam@gmail.com>
2022-11-12 00:52:55 +0530

    added routes and theme colors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route element={"hello this is home"} path="/" />
+          <Route element={"hello this is home"} path="/home" />
+          <Route element={"all blogs page"} path="/blogs" />
+          <Route
+            element={"hello this is profile/ your blog page"}
+            path="/dashboard"
+          />
+          <Route element={"hello this is create page"} path="/create" />
+          <Route element={"hello this is about page"} path="/about" />
+          <Route
+            element={"hello this is particular blog post page"}
+            path="/blog/id"
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
