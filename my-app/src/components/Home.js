855d11c528410686608a1527075165f75c87a3bf
Satyam <bajpai1973satyam@gmail.com>
2022-11-13 00:09:51 +0530

    fixed navbar

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -68,58 +68,58 @@ function Home() {
           <div className="col-sm-12 col-md-6 mt-4">
             <div
               id="carouselExampleInterval"
-              class="carousel slide"
+              className="carousel slide"
               data-bs-ride="carousel"
             >
-              <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
+              <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="10000">
                   <img
                     src={slider1}
-                    class="d-block w-100"
+                    className="d-block w-100"
                     alt=""
                     style={{ width: "100%", height: "100%" }}
                   />
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
+                <div className="carousel-item" data-bs-interval="2000">
                   <img
                     src={slider2}
-                    class="d-block w-100"
+                    className="d-block w-100"
                     alt=""
                     style={{ width: "100%", height: "100%" }}
                   />
                 </div>
-                <div class="carousel-item">
+                <div className="carousel-item">
                   <img
                     src={slider3}
-                    class="d-block w-100"
+                    className="d-block w-100"
                     alt=""
                     style={{ width: "100%", height: "100%" }}
                   />
                 </div>
               </div>
               <button
-                class="carousel-control-prev"
+                className="carousel-control-prev"
                 type="button"
                 data-bs-target="#carouselExampleInterval"
                 data-bs-slide="prev"
               >
                 <span
-                  class="carousel-control-prev-icon"
+                  className="carousel-control-prev-icon"
                   aria-hidden="true"
                 ></span>
-                <span class="visually-hidden">Previous</span>
+                <span className="visually-hidden">Previous</span>
               </button>
               <button
-                class="carousel-control-next"
+                className="carousel-control-next"
                 type="button"
                 data-bs-target="#carouselExampleInterval"
                 data-bs-slide="next"
               >
                 <span
-                  class="carousel-control-next-icon"
+                  className="carousel-control-next-icon"
                   aria-hidden="true"
                 ></span>
-                <span class="visually-hidden">Next</span>
+                <span className="visually-hidden">Next</span>
               </button>
             </div>
           </div>
--- my-app/src/components/common/Navbar.js
+++ my-app/src/components/common/Navbar.js
@@ -1,53 +1,72 @@
-import React from 'react'
-import { useState } from 'react';
-import { signOut } from 'firebase/auth'
+import React from "react";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
-import { Link } from 'react-router-dom';
-import "./styles/Navbar.css"
+import { Link } from "react-router-dom";
+// import "./styles/Navbar.css"
 
 const Navbar = () => {
-    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
-    
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
   const signUserOut = () => {
     signOut(auth).then(() => {
-      localStorage.clear()
-      setIsAuth(false)
+      localStorage.clear();
+      setIsAuth(false);
       window.location.pathname = "/login";
-    })
-  }
+    });
+  };
   return (
     <nav className="main-nav">
       <div className="logo">
-          <h2>
-            <span>Q</span>UARKS
-          </h2>
-          </div>
-        <div className ="menu-link">
+        <h2>
+          <span>Q</span>UARKS
+        </h2>
+      </div>
+
+      {/* <Link to="/"> Home </Link> */}
+
+      {!isAuth ? (
+        <div className="menu-link">
           <ul>
             <li>
-              < a href="#">Home</a>
+              {/* <a href="/">Home</a> */}
+              <Link to="/home">Home</Link>
             </li>
             <li>
-            < a href="#">About</a>
+              {/* <a href="/about">About</a> */}
+              <Link to="/home">About</Link>
             </li>
             <li>
-            < a href="#">Contact</a>
+              <Link to="/blogs"> Blogs </Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/create"> Create Post </Link>
+            </li>
+            <li>
+              <Link to="/login"> Login </Link>
             </li>
           </ul>
         </div>
+      ) : (
+        // <Link to="/login"> Login </Link>
+        <>
+          <div className="menu-link">
+            <ul>
+              <li>
+                <Link to="/createpost"> Create Post </Link>
+              </li>
+              <li>
+                <button onClick={signUserOut}> Log Out</button>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
+    </nav>
+  );
+};
 
-        <Link to="/"> Home </Link>
-
-        {!isAuth ? (
-          <Link to="/login"> Login </Link>
-        ) : (
-          <>
-            <Link to="/createpost"> Create Post </Link>
-            <button onClick={signUserOut}> Log Out</button>
-          </>
-        )}
-      </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
